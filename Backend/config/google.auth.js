c50bb016cdda3df36ac63f454a8b6ed70418f74b
yash-levi1896 <yashkumar18gupta@gmail.com>
2023-06-18 18:47:16 +0530

    localstorage data

--- Backend/config/google.auth.js
+++ Backend/config/google.auth.js
@@ -1,14 +1,15 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+require('dotenv').config();
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:8080/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+   // });
+   console.log(profile)
   }
 ));
 
