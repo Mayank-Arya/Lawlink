e102537c2e200b66de18dc882ac4fb74d635972c
yash-levi1896 <yashkumar18gupta@gmail.com>
2023-06-19 00:43:18 +0530

    lawyer login

--- Backend/config/google.auth.js
+++ Backend/config/google.auth.js
@@ -1,27 +1,30 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
 require('dotenv').config();
-const passport=require('passport')
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //   return cb(err, user);
-   // });
-  // console.log(profile)
-  done(null,profile)
-  }
-));
 
-passport.serializeUser((user,done)=>{
-  done(null,user)
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:8080/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // console.log(profile)//!--> consoling profile;
+      done(null, profile);
+    }
+  )
+);
+
+
+passport.serializeUser((user, done) => {
+  done(null, user);
 });
 
-passport.deserializeUser((user,done)=>{
-  done(null,user)
+passport.deserializeUser((user, done) => {
+  done(null, user);
 });
 
 
-module.exports=passport
+module.exports = passport;
